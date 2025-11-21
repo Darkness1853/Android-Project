598653debc819298e42e9e6ae149dac0aca1a827
Darkness1853 <petya.nguen@bk.ru>
2025-11-21 11:06:43 +0300

    Update 21.11.2025 Hub

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -16,25 +16,28 @@ class MainActivity : AppCompatActivity() {
             startActivity(calc_Intent)
         });
 
-//        findViewById<Button>(R.id.GoMedia).setOnClickListener({
-//            val calc_Intent = Intent(this, CalculatorActivity::class.java)
-//            startActivity(calc_Intent)
-//        });
-//
-//        findViewById<Button>(R.id.GoLocate).setOnClickListener({
-//            val calc_Intent = Intent(this, CalculatorActivity::class.java)
-//            startActivity(calc_Intent)
-//        });
-//
-//        findViewById<Button>(R.id.GoMobile_connect).setOnClickListener({
-//            val calc_Intent = Intent(this, CalculatorActivity::class.java)
-//            startActivity(calc_Intent)
-//        });
-//
-//        findViewById<Button>(R.id.GoSoket).setOnClickListener({
-//            val calc_Intent = Intent(this, CalculatorActivity::class.java)
-//            startActivity(calc_Intent)
-//        });
+        findViewById<Button>(R.id.GoMedia).setOnClickListener({
+            val Media_Intent = Intent(this, MediaActivity::class.java)
+            startActivity(Media_Intent)
+        });
+
+        findViewById<Button>(R.id.GoLocate).setOnClickListener({
+            val Locate_Intent = Intent(this, LocateActivity::class.java)
+            startActivity(Locate_Intent)
+        });
+
+        findViewById<Button>(R.id.GoMobile_connect).setOnClickListener({
+            val Mobile_Connect_Intent = Intent(this, Mobile_ConnectActivity::class.java)
+            startActivity(Mobile_Connect_Intent)
+        });
+
+        findViewById<Button>(R.id.GoSoket).setOnClickListener({
+            val Soket_Intent = Intent(this, SoketActivity::class.java)
+            startActivity(Soket_Intent)
+        });
+        findViewById<Button>(R.id.Exit).setOnClickListener ({
+            finishAffinity()
+        });
 
     }
 }
